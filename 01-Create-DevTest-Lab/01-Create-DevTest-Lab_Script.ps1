74016c3d4a39000a2aeab556c8073bc0bd6f920c
Personne 1 <KollyA05@studentfr.ch>
2021-05-27 09:16:37 +0200

    Update 01-Create-DevTest-Lab_Script.ps1
    Update file 01-Create-DevTest-Lab_Script.ps1

--- 01-Create-DevTest-Lab/01-Create-DevTest-Lab_Script.ps1
+++ 01-Create-DevTest-Lab/01-Create-DevTest-Lab_Script.ps1
@@ -6,36 +6,34 @@
 ###################################################################################################################################################
 
 #Variable for the creation of the devtest lab
-$name = ''
-$regionChoose = ''
-$resourceGroup = ''
+[string]$name = ''
+[string]$regionChoose = ''
+[string]$resourceGroup = ''
 
 #Variable to show the user the correct regions
 $locationNE = 'North Europe'
 $locationWE = 'West Europe'
 $locationSN = 'Switzerland North'
-$locationSW = 'Switzerland West'
 
 #Display of possibilities to the user 
 Write-Host "1 : "$locationNE
 Write-Host "2 : "$locationWE
 Write-Host "3 : "$locationSN
-Write-Host "4 : "$locationSW
 
 #Variable to test if one of the switch responses has been chosen.
-$ok = $true;
+$okLocation = $true;
 
 #Array for all region possibility
-$possibilityArray = 1..4
+$possibilityArray = 1..3
 
-#Loop as long as $ok is not equal to false. If $ok is equal to false, it means that one of the answers has been chosen and so we go to the next step
-While($ok){
+#Loop as long as $okLocation is not equal to false. If $okLocation is equal to false, it means that one of the answers has been chosen and so we go to the next step
+While($okLocation){
     #Reading the number entered by the user
-    $locationRead = Read-Host 'Choose a location (1- 4)'
+    $locationRead = Read-Host 'Choose a location (1- 3)'
     #Check if $locationRead value is in $possibilityArray
     if($locationRead -in $possibilityArray){
-        #Change of value in the variable $ok
-        $ok = $false
+        #Change of value in the variable $okLocation
+        $okLocation = $false
     }
 }
 #Verification of the return number and correspondence with the values imposed by Azure. If the number is not in the list, we start the while loop again
@@ -53,18 +51,36 @@ switch ($locationRead)
          #Change of value in the variable $regionChoose
          $regionChoose = "switzerlandnorth"
 		 break
-     } '4' {
-         #Change of value in the variable $regionChoose
-         $regionChoose = "switzerlandwest"
-		 break
      }
  }
 
-#Reading the name entered by the user and store in $name
-$name = Read-Host 'Enter the name of the new lab'
+#Variable to test if one of the name isn't "" or null.
+$okName = $true;
 
-#Reading the resource group entered by the user and store in $name
-$resourceGroup = Read-Host 'Enter the name of the resources group'
+#Loop as long as $okName is not equal to false. If $okName is equal to false, it means that one of the answers has been chosen and so we go to the next step
+While($okName){
+    #Reading the name entered by the user and store in $name
+    $name = Read-Host 'Enter the name of the new lab'
+    #Check if $name value isn't "" or null
+    if(-not [string]::IsNullOrEmpty($name)){
+        #Change of value in the variable $okName
+        $okName = $false
+    }
+}
+
+#Variable to test if one of the resource group isn't "" or null.
+$okResourceGroup = $true;
+
+#Loop as long as $okResourceGroup is not equal to false. If $okResourceGroup is equal to false, it means that one of the answers has been chosen and so we go to the next step
+While($okResourceGroup){
+    #Reading the resource group entered by the user and store in $resourceGroup
+    $resourceGroup = Read-Host 'Enter the name of the resources group'
+    #Check if $name value isn't "" or null
+    if(-not [string]::IsNullOrEmpty($resourceGroup)){
+        #Change of value in the variable $okResourceGroup
+        $okResourceGroup = $false
+    }
+}
 
 #Connect Azure Account
 Connect-AzAccount
